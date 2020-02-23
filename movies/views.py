079244d164f20818fa287f1acefbff60ea725e65
DF TCSS <dftcss@omnigon.com>
2020-02-23 19:44:51 +0300

    add api

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+
+from movies.models import Movie
+from .serializers import MovieSerializer
+
+
+class MovieAPiView(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class SingleMovieAPiView(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
