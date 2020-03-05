119d56bea51755620c95581f572ed9cad5dfa524
Denis Frunza <frunzadenis.93@gmail.com>
2020-03-05 21:38:34 +0300

    add Review model

--- app/movies/models.py
+++ app/movies/models.py
@@ -15,3 +15,11 @@ class Movie(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Review(models.Model):
+    user = models.CharField(max_length=255)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    comment = models.TextField(default="")
+    rating = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
